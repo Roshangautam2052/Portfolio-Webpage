6c3625e01893740cb12d7b15febfed83a0368ccb
Roshan Gautam <roshangautam2052@gmail.com>
2024-04-19 23:56:05 +0100

    pushing portfolio code in github rep

---
+++ js/script.js
@@ -0,0 +1,171 @@
+//===================================== DARK THEME =========================
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// PREVEIOSLY SELECTED TOPIC (checking from local storage)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme)?'dark':'light'
+const getCurrentIcon = () => document.body.classList.contains(iconTheme)?'uil-moon':'uil-sun'
+
+//We need to validate if the user has previously chosen a topic
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark'?'add':'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon'?'add':'remove'](iconTheme)
+}
+
+// Activate/ deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    //ADD or remove the dark/light icon -- icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    //We save the theme and the current icon that the user has chosen
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+console.log("THEME SETTING IS WORKING!")
+
+//===================================== MENU SHOW Y HIDDEN =========================
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+// ================  MENU SHOW  =============
+/*  Validate if the constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+} 
+
+// ================  MENU HIDE  =============
+/*  Validate if the constant exists */
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+} 
+
+
+
+console.log("MENU Y SETTING WORKING!")
+//===================================== REMOVE MENU PRORFILE =========================
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // when we click on nav__links, we remove the show menu
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+console.log("Remove menu profile is working!")
+
+
+//===================================== Typewriter Effect =========================
+
+
+new Typewriter('#typewriter', {
+  strings: ['Roshan Gautam', 'Software Developer ', 'Backend Developer',''],
+  autoStart: true,
+  loop: true,
+  cursor: "|"
+});
+console.log("Typewriter effect is working!")
+
+//===================================== Portfolio Swiper =========================
+
+var swiper = new Swiper(".blog-slider", {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel:{
+        invert: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".blog-slider__pagination",
+      clickable: true,
+    },
+    // mousewheel: true,
+    keyboard: true,
+  });
+console.log("Portfolio Swiper is working!")
+
+
+
+
+//===================================== SCROLL UP =========================
+function scrollUp(){
+    const scrollup = document.getElementById('scroll-up');
+    // When the scroll higher than 560 viewpoint /height , then the scroll up icon showld appear and on clicking should reach top of the page
+    if(this.scrollY >= 560) {
+        scrollup.classList.add('show-scroll');
+    }
+    else {
+        scrollup.classList.remove('show-scroll')
+    }
+    console.log("Scroll up being called and working!")
+}
+window.addEventListener('scroll', scrollUp)
+
+//===================================== SCROLL SECTION ACTIVE HIGHLIGHT =========================
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+
+
+    console.log("Section highlight working!")
+}
+window.addEventListener('scroll', scrollActive)
+
+//=========Setting up SMTP server for sending email ==================//
+function Send(){
+    var name = document.getElementById("Name").value;
+    var email = document.getElementById("Email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+
+    var body ="Name: "+ name + "<br/> Email: " + email  + "<br/> message: " + message;
+    console.log(body);
+    Email.send({
+    SecureToken : "39add5a4-00e7-4021-9f2c-ac45be2c8a1f",
+    To : 'roshangautam1876@gmail.com',
+    From : "roshangautam1876@gmail.com",
+    Subject : subject,
+    Body : body
+        }).then(
+        message => {
+            if(message=='OK'){
+                swal("Thank you!", " Your message has been sent successfully!", "success");
+            }
+            else{
+                swal("Oops!", "Oops! Something went Wrong, Try Again!", "error");
+            }
+        }
+    );
+    
+    
+}
+
+
